be31045f0c6010e360a87e6cb14e0f3fecf74da1
Georgiy Gubarkov <g.v.gubarkov@mpt.ru>
2021-12-30 10:02:22 +0300

    create tickets base and new migration create Certificate entity create Characteristic entity create Payment entity create migration with all entitys

--- backend/app/models.py
+++ backend/app/models.py
@@ -31,6 +31,33 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(45), unique=True)
 
+class Certificate(db.Model):    # Справки
+    __tablename__ = 'certificates'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    create_date = db.Column(db.Date, index=True, default=datetime.utcnow)
+    remove_date = db.Column(db.Date, index=True, nullable=True)
+    archive = db.Column(db.Boolean, default=False)
+    text = db.Column(db.String(300))
+
+class Characteristic(db.Model): # Характеристики
+    __tablename__ = 'characteristics'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    respondibilities = db.Column(db.String(300), nullable=True) # What doing in group
+    school = db.Column(db.String(150), nullable=False)
+    school_graduation = db.Column(db.String(5), nullable=False)
+    MPT_entrance = db.Column(db.String(5), nullable=False)
+    place = db.Column(db.String(100))
+    create_date = db.Column(db.Date, index=True, default=datetime.utcnow)
+    remove_date = db.Column(db.Date, index=True, nullable=True)
+    archive = db.Column(db.Boolean, default=False)
+
+class Payment(db.Model):    # Платёжки
+    __tablename__ = 'payments'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    payment = db.Column(db.String(300), nullable=False)
+    semestr = db.Column(db.String(45), nullable=False)
+    create_date = db.Column(db.Date, index=True, default=datetime.utcnow)
+
 @login.user_loader
 def load_user(id):
     return User.query.get(str(id))
